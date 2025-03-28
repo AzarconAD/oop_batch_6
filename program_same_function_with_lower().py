3c5d3ae23122466c88ed80ececb3e190e712b34e
Adam Daniel P. Azarcon <adamdanielpazarcon@iskolarngbayan.pup.edu.ph>
2025-03-28 13:21:43 +0800

    program 3 done

---
+++ program_same_function_with_lower().py
@@ -0,0 +1,6 @@
+# ask user input
+user_input = input("Enter anything: ").upper() # convert input to upperacase
+new_input = user_input.swapcase() # use swapcase
+
+# display output
+print(new_input)
