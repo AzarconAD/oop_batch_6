1031f55c6f314a17b06543122f38b225a6911cb9
Adam Daniel P. Azarcon <adamdanielpazarcon@iskolarngbayan.pup.edu.ph>
2025-03-28 22:21:19 +0800

    program 4 done

--- program_same_function_with_isupper.py
+++ program_same_function_with_isupper.py
@@ -1,3 +1,10 @@
 # ask user input
+user_input = input("Enter anything: ")
 # check each character if lowercase or uppercase
+def is_upper(string):
+    for char in string:
+        if "a" <= char <= "z":
+            return False
+    return True
 # display result
+print(is_upper(user_input))
