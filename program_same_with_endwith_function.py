b442cbe122c8851146766c71d356827b442726a1
Adam Daniel P. Azarcon <adamdanielpazarcon@iskolarngbayan.pup.edu.ph>
2025-03-28 22:34:30 +0800

    program 5 done

--- program_same_with_endwith_function.py
+++ program_same_with_endwith_function.py
@@ -1,4 +1,20 @@
 # ask user input and suffix to check
+user_input = input("Enter anything: ")
+suffix = input("Enter suffix to check: ")
+
+result = True # initializing resut
+
+input_len = len(user_input)
+suffix_len = len(suffix)
+
 # determine if suffix is longer that input
-# compare characters at the end
+if suffix_len > input_len:
+    result = False
+else:
+    for i in range(suffix_len): # compare characters at the end
+        if user_input[input_len - suffix_len + i] != suffix[i]:
+            result = False
+            break
+
 # display result
+print(result)
