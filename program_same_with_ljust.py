f1b62550d7a4e5f2388b1deb529e756aff0f3307
Adam Daniel P. Azarcon <adamdanielpazarcon@iskolarngbayan.pup.edu.ph>
2025-03-28 22:44:38 +0800

    program 8 done

--- program_same_with_ljust.py
+++ program_same_with_ljust.py
@@ -1,5 +1,11 @@
 # Ask for user input
+user_input = input("Enter a string: ")
+width = int(input("Enter the total width: "))
+
 # Check if the string is shorter than the width
+if len(user_input) < width:
 # Calculate how many spaces to add
 # Add spaces to the right of the string
+
 # display output
+print(user_input)
