d3504b34a70fc3928d1e3f87600da2731114e98d
Adam Daniel P. Azarcon <adamdanielpazarcon@iskolarngbayan.pup.edu.ph>
2025-03-28 12:21:31 +0800

    Update copying_lstrip_function_without_using_lstrip.py
    program 1 done

--- copying_lstrip_function_without_using_lstrip.py
+++ copying_lstrip_function_without_using_lstrip.py
@@ -1,4 +1,13 @@
 # ask user for input
-# detect if there is leading spaces in the input
-# remove leading spaces
+user_input = input("Enter anything with leading spaces: ")
+no_lead_spaces = ""
+
+found_char = False
+for char in user_input: # detect if there is leading spaces in the input
+    if char != " " and not found_char: 
+        found_char = True
+    if found_char:
+        no_lead_spaces += char
+       
 # display
+print(no_lead_spaces)
