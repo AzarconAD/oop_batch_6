4a7adc145c1e76da49cca8f48c37afb3f2d123f6
Adam Daniel P. Azarcon <adamdanielpazarcon@iskolarngbayan.pup.edu.ph>
2025-03-29 11:47:00 +0800

    program 1 done

--- batch_7/program_same_with_rstrip()_function.py
+++ batch_7/program_same_with_rstrip()_function.py
@@ -1,4 +1,9 @@
 # ask for user inpt
+user_input = input("Enter anything with trailing spaces: ")
+
 # detect if there is trailing spaces
-# remove trailing spaces
+while user_input[-1] == " ":
+    user_input = user_input[:-1] # remove trailing spaces
+
 # display output
+print(user_input)
