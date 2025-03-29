3f1ecd1601ff0b7cc7b2686fed31769f987db18a
Adam Daniel P. Azarcon <adamdanielpazarcon@iskolarngbayan.pup.edu.ph>
2025-03-29 12:17:29 +0800

    program 4 done

--- batch_7/program_same_with_islower().py
+++ batch_7/program_same_with_islower().py
@@ -1,3 +1,12 @@
 # ask user input 
+user_input = input("Enter anything: ")
+
 #check if input lowercase
+def is_lower(string):
+    for char in string:
+        if "A" <= char <= "Z":
+            return False
+    return True
+
 # display output
+print(is_lower(user_input))
