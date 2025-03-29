a03ffc41a002c76cc933814fb15ff5afdf5973e4
Adam Daniel P. Azarcon <adamdanielpazarcon@iskolarngbayan.pup.edu.ph>
2025-03-29 11:59:22 +0800

    program 2 done

--- batch_7/program_same_with_removesuffix()_function.py
+++ batch_7/program_same_with_removesuffix()_function.py
@@ -1,4 +1,12 @@
 # ask user for input and the suffix to remove
+user_input = input("Enter anything: ")
+suffix = input("Enter suffix to remove: ")
+
 # check if there is a suffix in the input
-# remove suffix
+if user_input.endswith(suffix):
+    user_input = user_input[:len(user_input) - len(suffix)] # remove suffix
+else:
+    user_input = user_input
+
 # display output
+print(user_input)
