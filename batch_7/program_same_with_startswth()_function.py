c2bcff020085d5e21bb8cd9c0f69c27950e2b556
Adam Daniel P. Azarcon <adamdanielpazarcon@iskolarngbayan.pup.edu.ph>
2025-03-29 12:30:22 +0800

    program 5 done

--- batch_7/program_same_with_startswth()_function.py
+++ batch_7/program_same_with_startswth()_function.py
@@ -1,3 +1,19 @@
 # ask user input and the preffix to check
-# compare input and prefix at the start
+user_input = input("Enter anything: ")
+prefix = input("Enter prefix to check: ")
+
+result = True
+
+input_len = len(user_input)
+prefix_len = len(prefix)
+
+if prefix_len > input_len:
+    result = False
+else:
+    for i in range(prefix_len): # compare input and prefix at the start
+        if user_input[i] != prefix[i]:
+            result = False
+            break
+
 # display result
+print(result)
