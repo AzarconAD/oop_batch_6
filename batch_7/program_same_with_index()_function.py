f32054d29ad3ab11281f18c1923fad358cb8db59
AzarconAD <200471951+AzarconAD@users.noreply.github.com>
2025-03-31 11:35:27 +0800

    program 9 pseudocode

---
+++ batch_7/program_same_with_index()_function.py
@@ -0,0 +1,7 @@
+# ask user for input
+# ask what substring to find
+# ask where to start 
+# ask where to end
+# loop in the input to find the substring
+# determine the index of the substring in the input
+# display the index
